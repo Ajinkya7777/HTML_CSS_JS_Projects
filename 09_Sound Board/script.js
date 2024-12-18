c1480ac8e1050e23197f89bb75dce8595cb7aca9
Ajinkya <shindeajinkya09.as@gmail.com>
2024-12-18 20:34:23 +0530

    commited 20 projects

---
+++ 09_Sound Board/script.js
@@ -0,0 +1,30 @@
+const sounds = [
+  "12AM",
+  "7Years",
+  "Bloodstream",
+  "HollywoodTonight",
+  "Perfect",
+  "WeOwnIt",
+];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
